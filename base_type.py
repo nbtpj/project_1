622c64b2436faab0960fbf887ac3a24b796daf67
Nguyen Minh Quang <gdra136@gmail.com>
2021-01-09 17:55:20 +0700

    add features of sentence

--- base_type.py
+++ base_type.py
@@ -25,19 +25,32 @@ class Sentence:
     NLP = StanfordCoreNLP(path_or_host=LINK_TO_CORE_NLP, memory='4g')
 
     def __init__(self, text='',
-                 bert=BERT, json=None, nlp = NLP):
+                 bert=BERT, json=None, nlp=NLP):
         self.nlp = nlp
         self.bert = bert
+        self.n_of_nouns = 0
+        self.n_of_numerals = 0
+        self.pos_in_para = None
+        self.tf_isf = None
         if json is None:
             temp_text = self.nlp.word_tokenize(text)
-            temp_text = ' '.join([self.STEMMER.stem(word) for word in temp_text if word not in self.STOPWORD])
+            self.tokens = [self.STEMMER.stem(word) for word in temp_text if word.lower() not in self.STOPWORD]
+            temp_text = ' '.join(self.tokens)
             self.word_vector = bert.word_vector(temp_text)
             self.sentence_vector = bert.sentence_vector(temp_text)
             self.text = text
         else:
             self.word_vector = json["word_vec"]
             self.sentence_vector = json["vector"]
+            temp_text = self.nlp.word_tokenize(json["text"])
+            self.tokens = [self.STEMMER.stem(word) for word in temp_text if word.lower() not in self.STOPWORD]
             self.text = json["text"]
+
+        for word, tag in self.pos_tag():
+            if tag[0] is 'N':
+                self.n_of_nouns += 1
+            if tag is 'CD':
+                self.n_of_numerals += 1
 
     def __str__(self):
         return self.text
@@ -82,6 +95,9 @@ class Sentence:
             "vector": self.sentence_vector.tolist()
         }
 
+    def __len__(self):
+        return len(self.tokens)
+
 
 class Question(Sentence):
     pass
@@ -89,10 +105,18 @@ class Question(Sentence):
 
 class Paragrapth:
     def __init__(self, text="", json=None):
+        self.tokens = []
+        self.sentences = []
         if json is None:
-            self.sentences = [Sentence(text=sen) for sen in tokenize.sent_tokenize(text)]
+            for sen in tokenize.sent_tokenize(text):
+                S = Sentence(text=sen)
+                self.sentences.append(S)
+                self.tokens.extend(S.tokens)
         else:
-            self.sentences = [Sentence(json=json[j]) for j in json]
+            for j in json:
+                S = Sentence(json=json[j])
+                self.sentences.append(S)
+                self.tokens.extend(S.tokens)
         self.iter = 0
 
     def __next__(self):
@@ -113,6 +137,7 @@ class Paragrapth:
                     self.sentences.append(sen)
             else:
                 raise TypeError(sent_para.__class__.__name__ + " can not be added with a Paragraph")
+        self.tokens.extend(sent_para.tokens)
         return self
 
     def __str__(self):
@@ -126,6 +151,9 @@ class Paragrapth:
 
     def __getitem__(self, key):
         return self.sentences[key]
+
+    def __len__(self):
+        return len(self.sentences)
 
 
 class Entry:
@@ -172,3 +200,6 @@ class Entry:
                      } for item in self.answers
                 ]
         }
+
+def if_isf():
+    ...
