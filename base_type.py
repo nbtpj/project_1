7652ad0f9cbc6063c38158aef60a4f1cd94d7088
Nguyen Minh Quang <gdra136@gmail.com>
2021-01-09 15:47:42 +0700

    + replace nltk by corenlp (at tokenize,tag_pos) + add 2 types of parsing

--- base_type.py
+++ base_type.py
@@ -1,98 +1,83 @@
 from biobert_embedding.embedding import BiobertEmbedding
-import nltk
 from nltk import tokenize
-# from nltk.corpus import state_union
-# from nltk.tokenize import PunktSentenceTokenizer
-# from nltk.corpus import treebank
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
 from nltk.stem.porter import *
 from nltk.corpus import stopwords
+import os
+from stanfordcorenlp import StanfordCoreNLP
+
+LINK_TO_CORE_NLP = r'C:\Users\Nguyen Minh Quang\Desktop\DS_lab\project_1\corenlp'
+
+
+def get_core_nlp():
+    os.chdir(LINK_TO_CORE_NLP)
+    os.system('java -mx8g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -timeout 5000')
+    return StanfordCoreNLP('http://localhost', port=9000)
 
 
 # open these lines if you run code first time
 # nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('averaged_perceptron_tagger')
-# nltk.download('conll2000')
-# nltk.download('maxent_ne_chunker')
-# nltk.download('words')
-# nltk.download('large_grammars')
-
-
-class Rule:
-    def match(self, sentence):
-        raise NotImplementedError
 
 
 class Sentence:
-    #  Văn phạm
-    GRAMMAR = nltk.data.load('grammars/large_grammars/atis.cfg')
     BERT = BiobertEmbedding()
     STEMMER = PorterStemmer()
     STOPWORD = stopwords.words('english')
+    NLP = StanfordCoreNLP(path_or_host=LINK_TO_CORE_NLP, memory='4g')
 
-    # khởi tạo với dữ liệu bert, text|json
     def __init__(self, text='',
-                 bert=BERT, json=None):
+                 bert=BERT, json=None, nlp = NLP):
+        self.nlp = nlp
         self.bert = bert
         if json is None:
-            temp_text = word_tokenize(text)
+            temp_text = self.nlp.word_tokenize(text)
             temp_text = ' '.join([self.STEMMER.stem(word) for word in temp_text if word not in self.STOPWORD])
             self.word_vector = bert.word_vector(temp_text)
             self.sentence_vector = bert.sentence_vector(temp_text)
-            self.text = word_tokenize(text)
+            self.text = text
         else:
             self.word_vector = json["word_vec"]
             self.sentence_vector = json["vector"]
-            self.text = word_tokenize(json["text"])
+            self.text = json["text"]
 
     def __str__(self):
-        return ' '.join(self.text)
+        return self.text
 
-    # gán nhãn cho mỗi từ
-    def tag(self):
-        return pos_tag(self.text)
+    def pos_tag(self):
+        """Part of speech tag"""
+        return self.nlp.pos_tag(self.__str__())
 
-    def chunking(self):
-        return nltk.ne_chunk(self.tag(), binary=True)
+    def constituency_parsing(self):
+        """Constituency Parsing"""
+        return self.nlp.parse(self.text)
 
-    # sử dụng tập văn phạm | luật (*rule) để đoán nhận câu
-    def get_form(self, *rules):
-        result = []
-        for rule in rules:
-            result.append(rule.match(self))
-        return result
+    def dependency_parsing(self):
+        """Dependency Parsing"""
+        return self.nlp.dependency_parse(self.text)
 
-    # biểu diễn câu
-    def visual(self):
-        nltk.ne_chunk(self.tag(), binary=True).draw()
+    def ner(self):
+        """Named-entity recognition"""
+        return self.nlp.ner(self.text)
 
-    # đánh dấu một số thành phần đặc biệt trong câu, lấy theo regex
-    def get_pattern(self, regex=r"""__________: {<.*>+}
-                            }<VB.?|IN|DT|TO>+{"""):
-        return nltk.RegexpParser(regex).parse(self.tag())
-
-    #     trả về 1 vector số đại diện cho câu có dạng sau :
-    #       ___________________768 chiều__________________________
-    #     [ tensor([ 1.0497e+00, -6.3529e-01, ....  , -2.0922e+00]),    |
-    #       tensor([ 1.0497e+00, -6.3529e-01, ....  , -2.0922e+00]),    |
-    #       .                                                           số từ trong câu (theo thứ tự)
-    #       .                                                           |
-    #       .                                                           |
-    #     ]
     def __abs__(self):
+        """
+        :return: a list of tensors:
+           ___________________768 dimensions__________________________
+        [ tensor([ 1.0497e+00, -6.3529e-01, ....  , -2.0922e+00]),    |
+          tensor([ 1.0497e+00, -6.3529e-01, ....  , -2.0922e+00]),    |
+          .                                                           number of words in the sentence
+          .                                                           |
+          .                                                           |
+        ]
+
+        """
         return self.word_vector
 
-    # trả dữ liệu về dạng dict:
-    # {
-    #     "text": câu đã được token và nối lại
-    #     "word_vec": vector từ
-    #     "vector":vector câu
-    # }
     def to_json(self):
+        """
+        :return:  an json-like format that can be dumped
+        """
         return {
-            "text": self.__str__(),
+            "text": self.text,
             "word_vec": [d.tolist() for d in self.word_vector],
             "vector": self.sentence_vector.tolist()
         }
@@ -102,9 +87,7 @@ class Question(Sentence):
     pass
 
 
-# đoạn văn : là tập hợp các câu
 class Paragrapth:
-    # Khởi tạo đoạn văn bản với dữ liệu text| json
     def __init__(self, text="", json=None):
         if json is None:
             self.sentences = [Sentence(text=sen) for sen in tokenize.sent_tokenize(text)]
@@ -120,8 +103,8 @@ class Paragrapth:
         except:
             raise StopIteration
 
-    # phép toán cộng đoạn văn với đoạn văn, đoạn văn với câu ̣làm thay đổi giá trị đoạn hiện tại
     def __add__(self, sent_para):
+        """increase the current paragraph by adding with a sentence of paragraph"""
         if isinstance(sent_para, Sentence):
             self.sentences.append(sent_para)
         else:
@@ -132,19 +115,12 @@ class Paragrapth:
                 raise TypeError(sent_para.__class__.__name__ + " can not be added with a Paragraph")
         return self
 
-    # nội dung dạng text của đoạn
     def __str__(self):
         return ' '.join([str(sentence) for sentence in self.sentences])
 
-    # tập hợp các vector câu trong đoạn
     def __abs__(self):
         return [sentence.sentence_vector.tolist() for sentence in self.sentences]
 
-    # trả dữ liệu dưới dạng list:
-    # [
-    #     câu 1,
-    #     câu 2,...
-    # ]
     def to_json(self):
         return [sent.to_json() for sent in self.sentences]
 
@@ -153,6 +129,8 @@ class Paragrapth:
 
 
 class Entry:
+    """A copy of raw data, has been processed (create new vectors)"""
+
     def __init__(self, json):
         self.question = Paragrapth(json["question"])
         self.multi_abs_summ = Paragrapth(json["multi_abs_summ"])
@@ -165,27 +143,22 @@ class Entry:
              } for item in json["answers"]
         ]
 
-    def __str__(self):
-        return '"question": {} \n"multi_abs_summ": {}\n"answer_ext_summ": {}'.format(str(self.question),
-                                                                                     str(self.multi_abs_summ),
-                                                                                     str(self.multi_ext_summ))
-
-    # trả dữ liệu dưới dạng dict:
-    # {
-    #     "question": đoạn question
-    #     "multi_abs_summ": đoạn tóm tắt abstract đa văn bản
-    #     "multi_ext_summ": đoạn tóm tắt extract đa văn bản
-    #     "answers": [
-    #         {
-    #             "answer_abs_summ": đoạn tóm tắt abstract đơn văn bản
-    #             "answer_ext_summ": đoạn tóm tắt extract đơn văn bản
-    #             "article": nội dung văn bản
-    #             "rating": đánh giá
-    #             <do section được làm thủ công nên em không đưa vào dữ liệu>
-    #         }
-    #     ]
-    # }
     def to_json(self):
+        """ :return: a json-like format that can be dumped
+        {
+            "question": __paragraph__
+            "multi_abs_summ": __paragraph__
+            "multi_ext_summ": __paragraph__
+            "answers": [
+                {
+                    "answer_abs_summ": __paragraph__
+                    "answer_ext_summ": __paragraph__
+                    "article": __paragraph__
+                    "rating": __text__
+                }
+            ]
+        }
+        """
         return {
             "question": self.question.to_json(),
             "multi_abs_summ": self.multi_abs_summ.to_json(),
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,16 @@
-from base_type import Entry
+from base_type import Entry,Sentence
 import json
 from sklearn.cluster import AgglomerativeClustering
 from base_type import Paragrapth
 import os
+
 
 LINK_TO_INPUT = 'DATA/question_driven_answer_summarization_primary_dataset.json'
 LINK_TO_OUTPUT = 'DATA/question_driven_answer_summarization_primary_dataset_output.json'
 LINK_TO_TEMPLATE_FILE = 'DATA/template.txt'
 
 
-# phâp cụm phâm cấp: 1 đoạn văn -> n đoạn văn
-def para_clustering(para, clusters=3, distance='euclidean'):
+def para_clustering(para, clusters=3, distance='cosine'):
     clustering = AgglomerativeClustering(n_clusters=clusters, affinity=distance).fit(abs(para))
     result = [Paragrapth() for i in range(clusters)]
     count = -1
@@ -22,9 +22,9 @@ def para_clustering(para, clusters=3, distance='euclidean'):
 
 class Preprocessing:
     data = json.load(open(LINK_TO_INPUT))
-    output = []
 
     def __init__(self):
+        """loading data. if it is not available, it processes data, then store it to file"""
         self.output = []
         if os.path.exists(LINK_TO_OUTPUT) and os.path.getsize(LINK_TO_OUTPUT) > 0:
             try:
@@ -39,3 +39,4 @@ class Preprocessing:
                 json.dump(self.output, f)
                 f.close()
             print("successfully saving data!")
+        Sentence.NLP.close()
